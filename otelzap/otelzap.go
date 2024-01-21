16df32cfe7cbb757181f29ec9ec0091fdfee28ca
Vincent Batoufflet <vincent@batoufflet.info>
2024-01-21 04:43:21 +0100

    feat: Add minimal log level support to OpenTelemetry Core (#11)

--- otelzap/otelzap.go
+++ otelzap/otelzap.go
@@ -22,13 +22,29 @@ import (
 )
 
 // NewOtelCore creates new OpenTelemetry Core to export logs in OTLP format
-func NewOtelCore(loggerProvider otel.LoggerProvider) zapcore.Core {
+func NewOtelCore(loggerProvider otel.LoggerProvider, opts ...Option) zapcore.Core {
 	logger := loggerProvider.Logger(
 		instrumentationScope.Name,
 		otel.WithInstrumentationVersion(instrumentationScope.Version),
 	)
 
-	return &otlpCore{
+	c := &otlpCore{
 		logger: logger,
+		level:  zapcore.InfoLevel,
 	}
+	for _, apply := range opts {
+		apply(c)
+	}
+
+	return c
+}
+
+// Option is a function that applies an option to an OpenTelemetry Core
+type Option func(c *otlpCore)
+
+// WithLevel sets the minimum level for the OpenTelemetry Core log to be exported
+func WithLevel(level zapcore.Level) Option {
+	return Option(func(c *otlpCore) {
+		c.level = level
+	})
 }
